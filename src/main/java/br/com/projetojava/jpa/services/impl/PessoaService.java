53adb9b0f1eeba88e202741ec9425655cf6b3c76
Johab <freitasjohab@gmail.com>
2022-12-23 19:14:39 -0300

    Criando Interfaces, services e implementações

---
+++ src/main/java/br/com/projetojava/jpa/services/impl/PessoaService.java
@@ -0,0 +1,40 @@
+package br.com.projetojava.jpa.services.impl;
+
+import java.util.List;
+
+import br.com.projetojava.jpa.models.Pessoa;
+import br.com.projetojava.jpa.services.interfaces.CrudService;
+
+public class PessoaService implements CrudService<Pessoa, Integer>{
+
+	@Override
+	public List<Pessoa> all() {
+		return null;
+	}
+
+	@Override
+	public Pessoa byId(Integer id) {
+		return null;
+	}
+
+	@Override
+	public Pessoa insert(Pessoa entity) {
+		return null;
+	}
+
+	@Override
+	public Pessoa update(Pessoa entity) {
+		return null;
+	}
+
+	@Override
+	public void delete(Pessoa entity) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+}
