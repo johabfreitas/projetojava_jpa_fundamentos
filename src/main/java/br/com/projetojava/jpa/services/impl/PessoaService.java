e7d3bcf6552f717cb3efc217e13f4125a2d0af6d
Johab <freitasjohab@gmail.com>
2022-12-23 19:56:49 -0300

    Criando tabela com JPA

--- src/main/java/br/com/projetojava/jpa/services/impl/PessoaService.java
+++ src/main/java/br/com/projetojava/jpa/services/impl/PessoaService.java
@@ -1,15 +1,29 @@
 package br.com.projetojava.jpa.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
 
 import br.com.projetojava.jpa.models.Pessoa;
 import br.com.projetojava.jpa.services.interfaces.CrudService;
+import br.com.projetojava.jpa.utils.JpaUtils;
 
 public class PessoaService implements CrudService<Pessoa, Integer>{
 
 	@Override
 	public List<Pessoa> all() {
-		return null;
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		EntityManager em = null;
+		try {
+			em  = JpaUtils.getEntityManager();
+			pessoas = em.createQuery("from Pessoa", Pessoa.class).getResultList();
+			return pessoas;
+		} finally {
+			if(em != null) {
+				em.close();
+			}
+		}
 	}
 
 	@Override
